bb8e80a9c1f0bc0a6d6f2ad216e30360b5161ba2
kazijamal <jamalkazi02@gmail.com>
2021-07-28 20:08:06 -0400

    Move HighlightsForm to its own component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,37 +1,18 @@
-import React, { useRef } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
+import HighlightsForm from "../HighlightsForm/HighlightsForm";
 
-const App = () => {
-	const formRef = useRef(null);
-	const submit_file = (e) => {
-		e.preventDefault();
-
-		const form_data = new FormData(formRef.current);
-
-		axios({
-			url: "http://localhost:3030/highlights/upload",
-			method: "post",
-			headers: { "Content-Type": "multipart/form-data" },
-			data: form_data,
-		})
-			.then((res) => console.log(res))
-			.catch((err) => console.error(err));
-	};
-
+function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<form onSubmit={submit_file} ref={formRef}>
-					<input type="file" name="highlights-file" />
-					<input type="submit" value="Submit" />
-				</form>
+				<HighlightsForm />
 			</header>
 		</div>
 	);
-};
+}
 
 export default App;
---
+++ client/src/components/HighlightsForm/HighlightsForm.js
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import axios from "axios";
+
+const HighlightsForm = () => {
+	const formRef = useRef(null);
+	const submit_file = (e) => {
+		e.preventDefault();
+
+		const form_data = new FormData(formRef.current);
+
+		axios({
+			url: "http://localhost:3030/highlights/upload",
+			method: "post",
+			headers: { "Content-Type": "multipart/form-data" },
+			data: form_data,
+		})
+			.then((res) => console.log(res))
+			.catch((err) => console.error(err));
+	};
+
+	return (
+		<form onSubmit={submit_file} ref={formRef}>
+			<input type="file" name="highlights-file" />
+			<input type="submit" value="Submit" />
+		</form>
+	);
+};
+
+export default HighlightsForm;
