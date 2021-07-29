d8daa773943a0390081b876b99479bed69aedf1d
kazijamal <jamalkazi02@gmail.com>
2021-07-28 20:10:44 -0400

    Remove unnecessary styling in App

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -6,9 +6,6 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
 				<HighlightsForm />
 			</header>
 		</div>
