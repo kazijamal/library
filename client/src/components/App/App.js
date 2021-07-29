c7a7bfb3d16ce99b787eebfeb48446d3c0a85ea2
kazijamal <jamalkazi02@gmail.com>
2021-07-28 20:03:43 -0400

    Create components organization in client

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
@@ -23,18 +22,9 @@ const App = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
 				<form onSubmit={submit_file} ref={formRef}>
 					<input type="file" name="highlights-file" />
 					<input type="submit" value="Submit" />
