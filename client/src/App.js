514d13129b291f3fda8c5a4f350a312e7d6920cf
kazijamal <jamalkazi02@gmail.com>
2021-07-31 01:17:31 -0400

    Update client directory structure and remove unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./views/Home/Home";
-import Login from "./views/Authentication/Login";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Dashboard from "./components/Dashboard/Dashboard";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
 	return (
