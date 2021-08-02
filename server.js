97d7f1e128ca03be10fe74dabe970f67b484acc3
kazijamal <jamalkazi02@gmail.com>
2021-08-02 19:02:42 -0400

    Change process.env.port to process.env.PORT and add npm run dev script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-const port = process.env.port || 3030;
+const port = process.env.PORT || 3030;
 
 const app = express();
 
