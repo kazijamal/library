6a768fc16be26d518d3644dc62ecf627bc1160bf
kazijamal <jamalkazi02@gmail.com>
2021-08-01 20:28:29 -0400

    Add API routes for getting a highlight with id and getting all highlights for a finished book

--- server/routes/highlights.js
+++ server/routes/highlights.js
@@ -7,11 +7,30 @@ const prisma = require("../lib/prisma");
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
+// get all highlights
 router.get("/", async (req, res) => {
 	const highlights = await prisma.highlight.findMany();
 	res.json(highlights);
 });
 
+// get highlight with id
+router.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	const highlight = await prisma.highlight.findUnique({
+		where: { id: Number(id) },
+	});
+	res.json(highlight);
+});
+
+// get highlights for finished book with id
+router.get("/finishedbook/:finishedBookId", async (req, res) => {
+	const { finishedBookId } = req.params;
+	const highlights = await prisma.highlight.findMany({
+		where: { finishedBookId: Number(finishedBookId) },
+	});
+});
+
+// upload highlights for a finished book
 router.post("/upload", upload.single("highlights-file"), async (req, res) => {
 	highlightsJSON = await toJSON(String(req.file.buffer));
 	const finishedBookId = Number(req.body.finishedBookId);
