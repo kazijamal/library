a87f707f6db0fe5859852634427281b91b27bab3
kazijamal <jamalkazi02@gmail.com>
2021-12-22 17:59:29 -0500

    Style login page

--- client/src/components/Home/ReadingBookList/ReadingBookList.js
+++ client/src/components/Home/ReadingBookList/ReadingBookList.js
@@ -19,7 +19,7 @@ function ReadingBookList() {
 		<div className="my-5">
 			<h3 className="text-2xl font-semibold my-3">Reading Books</h3>
 			{readingBooks ? (
-				<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+				<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
 					{readingBooks.map((readingBook) => (
 						<Link to={`/readingbooks/${readingBook.id}`} key={readingBook.id} className="rounded-lg mx-5 my-3 p-4 shadow-lg transition ease-in-out delay-150 hover:scale-105 hover:shadow-2xl">
 							<img src={readingBook.imageLink} alt="book cover" className="m-auto mb-3 w-full" />
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -23,27 +23,29 @@ function Login() {
 	}
 
 	return (
-		<div>
-			<h1>Log In</h1>
+		<div className="rounded-2xl shadow-xl m-auto p-10 w-3/4 md:w-1/2">
+			<h1 className="text-3xl font-semibold text-center mb-5">Log In</h1>
 			<form onSubmit={handleSubmit}>
 				<label>
-					<p>Username</p>
+					<p className="text-lg font-medium">Username</p>
 					<input
 						type="text"
 						onChange={(e) => setUsername(e.target.value)}
 						required
+						className="my-3 px-3 py-2 border border-slate-300 rounded-md w-full"
 					/>
 				</label>
 				<label>
-					<p>Password</p>
+					<p className="text-lg font-medium">Password</p>
 					<input
 						type="password"
 						onChange={(e) => setPassword(e.target.value)}
 						required
+						className="my-3 px-3 py-2 border border-slate-300 rounded-md w-full"
 					/>
 				</label>
-				<div>
-					<button type="submit">Submit</button>
+				<div className="grid place-items-center">
+					<button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mt-5 mx-auto rounded">Submit</button>
 				</div>
 			</form>
 		</div>
