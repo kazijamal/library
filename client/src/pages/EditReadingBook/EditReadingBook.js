08f0c28e666ea0184da657db921813b51c350738
kazijamal <jamalkazi02@gmail.com>
2021-12-24 19:42:36 -0500

    Style edit reading book page

--- client/src/pages/EditReadingBook/EditReadingBook.js
+++ client/src/pages/EditReadingBook/EditReadingBook.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import {
 	getReadingBook,
 	markReadingBookFinished,
@@ -38,34 +38,39 @@ function EditReadingBook() {
 
 	return (
 		<div>
-			<img src={readingBook.imageLink} alt="book cover" />
-			<h2>Editing {readingBook.title}</h2>
-			{readingBook.subtitle && <h3>{readingBook.subtitle}</h3>}
-			<h4>{readingBook.authors.join(", ")}</h4>
-			<p>
-				Categories:{" "}
-				{readingBook.categories.length ? (
-					readingBook.categories.join(", ")
-				) : (
-					<span>None</span>
-				)}
-			</p>
-			{readingBook.pageCount && <p>{readingBook.pageCount} pages</p>}
-			<form onSubmit={handleMarkReadingBookFinished}>
-				<label>
-					<p>Date Finished</p>
-					<input
-						type="date"
-						onChange={(e) => setDateFinished(e.target.value)}
-						value={dateFinished}
-						required
-					/>
-				</label>
-				<button type="submit">Mark Reading Book as Finished</button>
-			</form>
-			<button onClick={() => handleDeleteReadingBook(readingBook.id)}>
-				Delete Reading Book
-			</button>
+			<Link to="/dashboard" className="text-xl underline text-gray-600 hover:text-black">← Back to dashboard</Link>
+			<div className="text-center mt-5">
+				<img src={readingBook.imageLink} alt="book cover" className="m-auto shadow-lg" />
+				<h2 className="text-3xl font-semibold mt-5">{readingBook.title}</h2>
+				{readingBook.subtitle && <h3 className="text-xl font-medium italic">{readingBook.subtitle}</h3>}
+				<h4 className="text-xl font-medium mt-3">{readingBook.authors.join(", ")}</h4>
+				<p>
+					Categories:{" "}
+					{readingBook.categories.length ? (
+						readingBook.categories.join(", ")
+					) : (
+						<span>None</span>
+					)}
+				</p>
+				{readingBook.pageCount && <p>{readingBook.pageCount} pages</p>}
+				<form onSubmit={handleMarkReadingBookFinished} className="my-5">
+					<label>
+						<p className="text-xl font-medium mt-3">Date Finished</p>
+						<input
+							type="date"
+							onChange={(e) => setDateFinished(e.target.value)}
+							value={dateFinished}
+							required
+							className="my-3 px-3 py-2 border border-gray-300 rounded-md"
+						/>
+					</label>
+					<br />
+					<button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Mark Reading Book as Finished</button>
+				</form>
+				<button onClick={() => handleDeleteReadingBook(readingBook.id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+					Delete Reading Book
+				</button>
+			</div>
 		</div>
 	);
 }
