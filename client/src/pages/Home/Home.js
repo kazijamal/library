3f834e39151f7cbcdae3a56ed91514e42f053d4f
kazijamal <jamalkazi02@gmail.com>
2022-01-08 19:06:55 -0500

    Remove login button

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,16 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ReadingBookList from "../../components/Home/ReadingBookList/ReadingBookList";
-import FinishedBookList from "../../components/Home/FinishedBookList/FinishedBookList";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ReadingBookList from '../../components/Home/ReadingBookList/ReadingBookList';
+import FinishedBookList from '../../components/Home/FinishedBookList/FinishedBookList';
 
 function Home() {
-	return (
-		<div>
-			<Link to="/login" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Log In</Link>
-			<ReadingBookList />
-			<FinishedBookList />
-		</div>
-	);
+  return (
+    <div>
+      <ReadingBookList />
+      <FinishedBookList />
+    </div>
+  );
 }
 
 export default Home;
