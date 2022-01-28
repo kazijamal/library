e48307592ee94e716a6f94b2ae97d532a7336814
kazijamal <jamalkazi02@gmail.com>
2022-01-28 12:45:42 -0500

    Add dark mode toggle

--- client/src/App.js
+++ client/src/App.js
@@ -13,13 +13,51 @@ import AddReadingBook from './pages/AddReadingBook/AddReadingBook';
 import EditFinishedBook from './pages/EditFinishedBook/EditFinishedBook';
 import AddFinishedBook from './pages/AddFinishedBook/AddFinishedBook';
 import NotFound from './pages/NotFound/NotFound';
+import { useDarkMode } from './utils/useDarkMode';
 
 function App() {
+  const [isDark, setIsDark] = useDarkMode();
+
   return (
-    <div className='wrapper'>
+    <div className='relative wrapper'>
       <h1 className='select-none mb-5 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-purple-600'>
         Kazi's Library
       </h1>
+      <div className='absolute top-6 right-6 w-min cursor-pointer transition ease-in-out delay-50 hover:scale-110'>
+        {isDark ? (
+          <svg
+            onClick={(e) => setIsDark(false)}
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-10 w-10 text-indigo-200'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z'
+            />
+          </svg>
+        ) : (
+          <svg
+            onClick={(e) => setIsDark(true)}
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-10 w-10 text-gray-900'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+            />
+          </svg>
+        )}
+      </div>
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
---
+++ client/src/utils/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useSafeLocalStorage } from './useSafeLocalStorage';
+
+export function useDarkMode() {
+  const [isEnabled, setIsEnabled] = useSafeLocalStorage('dark-mode', undefined);
+
+  const enabled = isEnabled === undefined ? false : isEnabled;
+
+  useEffect(() => {
+    if (window === undefined) return;
+    const root = window.document.documentElement;
+    root.classList.remove(enabled ? 'light' : 'dark');
+    root.classList.add(enabled ? 'dark' : 'light');
+  }, [enabled]);
+
+  return [enabled, setIsEnabled];
+}
