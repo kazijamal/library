3c8f6d575b2d8ac5cb0c1c9fb7f2be48ce0eb87d
kazijamal <jamalkazi02@gmail.com>
2022-01-30 16:52:20 -0500

    Revert hover effects change

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      screens: {
-        betterhover: { raw: '(hover: hover)' },
-      },
-    },
+    extend: {},
   },
   plugins: [],
   darkMode: 'class',
