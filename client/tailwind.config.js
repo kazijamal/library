06c22c9bb567d6cebb96dff92d5bc6fe47e594bb
kazijamal <jamalkazi02@gmail.com>
2022-01-30 16:47:15 -0500

    Remove hover effects on mobile

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        betterhover: { raw: '(hover: hover)' },
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
