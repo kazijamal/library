f62e5c808546fc5199c74bf96a2bd7410a44fe1f
kazijamal <jamalkazi02@gmail.com>
2023-01-27 23:37:17 -0500

    Fix https error

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3030/',
+      '/api': {
+        target: 'http://localhost:3030/',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
