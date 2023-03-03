9b2df5b22ef4eb35d245eaff7c1dfd6a2fec08e4
kazijamal <jamalkazi02@gmail.com>
2023-03-03 16:13:19 -0500

    Add skeleton for finished book highlight list

--- client/src/components/Highlight.jsx
+++ client/src/components/Highlight.jsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
 
-function Highlight({ highlight }) {
+function Highlight({ highlight, includeBook }) {
   return (
     <div>
       <p className="mx-2 mt-5 mb-2 rounded bg-indigo-100 p-2 dark:bg-indigo-500/25">
         {highlight.content}
       </p>
-      <p className="text-md mx-3 mb-5">
-        Highlight from{" "}
-        <Link
-          to={`/finishedbooks/${highlight.finishedBook.id}`}
-          className="font-medium hover:underline dark:hover:text-indigo-200"
-        >
-          {highlight.finishedBook.title}
-        </Link>
-      </p>
+      {includeBook && (
+        <p className="text-md mx-3 mb-5">
+          Highlight from{" "}
+          <Link
+            to={`/finishedbooks/${highlight.finishedBook.id}`}
+            className="font-medium hover:underline dark:hover:text-indigo-200"
+          >
+            {highlight.finishedBook.title}
+          </Link>
+        </p>
+      )}
     </div>
   );
 }
