29835640f2d22876aecf7b7566caccda69510ff1
kazijamal <jamalkazi02@gmail.com>
2023-03-06 11:55:58 -0500

    Extract book card component and change styling

---
+++ client/src/components/BookCard.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+const getLink = (book, type, dashboard) => {
+  if (type == "reading-book") {
+    return dashboard
+      ? `/dashboard/readingbooks/edit/${book.id}`
+      : `/readingbooks/${book.id}`;
+  } else if (type == "finished-book") {
+    return dashboard
+      ? `/dashboard/finishedbooks/edit/${book.id}`
+      : `/finishedbooks/${book.id}`;
+  }
+};
+
+function BookCard({ book, type, dashboard }) {
+  return (
+    <Link
+      to={getLink(book, type, dashboard)}
+      key={book.id}
+      className="mx-5 my-3 rounded-lg border border-neutral-700 bg-neutral-800 p-4 transition delay-150 ease-in-out hover:bg-indigo-500/10"
+    >
+      <img
+        src={book.imageLink}
+        alt="book cover"
+        className="m-auto mb-3 w-full rounded-lg"
+      />
+      <p className="text-md text-center font-medium">{book.title}</p>
+    </Link>
+  );
+}
+
+export default BookCard;
--- client/src/components/ReadingBookList.jsx
+++ client/src/components/ReadingBookList.jsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getReadingBooks } from "../services/readingBooks";
 import BookListSkeleton from "./Skeleton/BookListSkeleton";
+import BookCard from "./BookCard";
 
 function ReadingBookList({ dashboard }) {
   const {
@@ -21,24 +21,12 @@ function ReadingBookList({ dashboard }) {
       {readingBooks && (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
           {readingBooks.map((readingBook) => (
-            <Link
-              to={
-                dashboard
-                  ? `/dashboard/readingbooks/edit/${readingBook.id}`
-                  : `/readingbooks/${readingBook.id}`
-              }
+            <BookCard
               key={readingBook.id}
-              className="mx-5 my-3 rounded-lg bg-neutral-800 p-4 shadow-lg transition delay-150 ease-in-out hover:scale-105 hover:shadow-2xl"
-            >
-              <img
-                src={readingBook.imageLink}
-                alt="book cover"
-                className="m-auto mb-3 w-full rounded-lg"
-              />
-              <p className="text-md text-center font-medium">
-                {readingBook.title}
-              </p>
-            </Link>
+              book={readingBook}
+              type="reading-book"
+              dashboard={dashboard}
+            />
           ))}
         </div>
       )}
