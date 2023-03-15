7da8ed169462e18c464168605efd3ccb9c9b3162
kazijamal <jamalkazi02@gmail.com>
2023-03-15 13:39:59 -0400

    Add types to skeletons

--- client/src/components/Skeleton/BookListSkeleton.tsx
+++ client/src/components/Skeleton/BookListSkeleton.tsx
@@ -1,6 +1,10 @@
 import BookSkeleton from "./BookSkeleton";
 
-function BookListSkeleton({ numBooks }) {
+type BookListSkeletonProps = {
+  numBooks: number;
+};
+
+function BookListSkeleton({ numBooks }: BookListSkeletonProps) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
       {[...Array(numBooks).keys()].map((n) => (
