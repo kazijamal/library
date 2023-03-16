d7a683ceb94c03089613bcce1609e66e018482ab
kazijamal <jamalkazi02@gmail.com>
2023-03-16 14:31:51 -0400

    Fix user token types

--- client/src/utils/useToken.ts
+++ client/src/utils/useToken.ts
@@ -3,13 +3,15 @@ import { useState } from "react";
 export default function useToken() {
   const getToken = () => {
     const tokenString = localStorage.getItem("token");
-    const userToken = JSON.parse(tokenString);
-    return userToken?.token;
+    if (tokenString) {
+      const userToken = JSON.parse(tokenString);
+      return userToken?.token;
+    }
   };
 
   const [token, setToken] = useState(getToken());
 
-  const saveToken = (userToken) => {
+  const saveToken = (userToken: { token: string }) => {
     localStorage.setItem("token", JSON.stringify(userToken));
     setToken(userToken.token);
   };
